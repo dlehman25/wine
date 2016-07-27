5d2fe71f75fdfbf0ac52c4a65b1d0fcb50904b7c
Daniel Lehman <dlehman25@gmail.com>
2016-07-26 22:43:18 -0700

    Revert "msvcrt: Don't use default process heap."
    This reverts commit d48314c8a50a69538985a8ce273fba1611585fbe.
    
    Conflicts:
    	dlls/msvcrt/heap.c
    
    a commit was made in vanilla Wine to give each version of msvcrt its own heap
    this mirrors what Microsoft did until vs2012, when it started using the process heap
    
    this revert not only more closely matches Microsoft does now but also affects
    overall memory usage.  the new heap preallocates some memory for performance
    having multiple heaps means that some memory is wasted - preallocated blocks
    on one heap can't be used by another.  having all requests use the same area
    of memory reduces that waste
    
    Signed-off-by: Daniel Lehman <dlehman25@gmail.com>

--- dlls/msvcrt/msvcrt.h
+++ dlls/msvcrt/msvcrt.h
@@ -341,8 +341,6 @@ extern void msvcrt_free_args(void) DECLSPEC_HIDDEN;
 extern void msvcrt_init_signals(void) DECLSPEC_HIDDEN;
 extern void msvcrt_free_signals(void) DECLSPEC_HIDDEN;
 extern void msvcrt_free_popen_data(void) DECLSPEC_HIDDEN;
-extern BOOL msvcrt_init_heap(void) DECLSPEC_HIDDEN;
-extern void msvcrt_destroy_heap(void) DECLSPEC_HIDDEN;
 
 #if _MSVCR_VER >= 100
 extern void msvcrt_init_scheduler(void*) DECLSPEC_HIDDEN;
