84fa112a0147c6e6dd30620fb938f850e18270da
Daniel Lehman <dlehman@esri.com>
2018-06-04 18:28:07 -0700

    ntdll: llheap: Implement RtlSetHeapInformation.
    Signed-off-by: Daniel Lehman <dlehman@esri.com>

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -3328,6 +3328,7 @@ void WINAPI LdrInitializeThunk( void *kernel_start, ULONG_PTR unknown2,
     static const WCHAR globalflagW[] = {'G','l','o','b','a','l','F','l','a','g',0};
     NTSTATUS status;
     WINE_MODREF *wm;
+    ULONG info;
     PEB *peb = NtCurrentTeb()->Peb;
 
     kernel32_start_process = kernel_start;
@@ -3352,6 +3353,8 @@ void WINAPI LdrInitializeThunk( void *kernel_start, ULONG_PTR unknown2,
     LdrQueryImageFileExecutionOptions( &peb->ProcessParameters->ImagePathName, globalflagW,
                                        REG_DWORD, &peb->NtGlobalFlag, sizeof(peb->NtGlobalFlag), NULL );
     heap_set_debug_flags( GetProcessHeap() );
+    info = 2;
+    RtlSetHeapInformation( GetProcessHeap(), HeapCompatibilityInformation, &info, sizeof(info) );
 
     /* the main exe needs to be the first in the load order list */
     RemoveEntryList( &wm->ldr.InLoadOrderModuleList );
