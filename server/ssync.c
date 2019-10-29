1885d06430cdd5c8c8f88fa3f8e9cf7e103c69ef
Daniel Lehman <dlehman25@gmail.com>
2019-10-28 23:32:34 -0700

    server: server side obj

--- server/ssync.c
+++ server/ssync.c
@@ -1,5 +1,5 @@
 /*
- * Server-side shared memory synchronization 
+ * Server-side shared memory synchronization
  *
  * Copyright (C) 2019 Daniel Lehman
  *
@@ -24,6 +24,7 @@
 #include "wine/list.h"
 #include "wine/shmlib.h"
 #include "shmlib.h"
+#include "ssync.h"
 
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -32,9 +33,19 @@
 
 struct ss_obj *ss_alloc(void)
 {
-    return NULL;
+    shm_ptr_t shm_ptr;
+    struct ss_obj *obj;
+
+    if ((shm_ptr = shm_malloc(sizeof(*obj))) == SHM_NULL)
+        return NULL;
+
+    obj = shm_ptr_to_void_ptr(shm_ptr);
+    obj->shm_ptr = shm_ptr;
+    return obj;
 }
 
 void ss_free(struct ss_obj *obj)
 {
+    if (!obj) return;
+    shm_free(obj->shm_ptr);
 }
