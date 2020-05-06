87aff6844a0ab71e9a0cdb7ff1ca2187fee09161
Daniel Lehman <dlehman25@gmail.com>
2020-05-06 10:23:24 -0700

    msspellcheckingfacility/tests: Add provider test dll stub.

---
+++ dlls/msspellcheckingfacility/tests/testprov.c
@@ -0,0 +1,28 @@
+/*
+ * Test Spell Check Provider
+ *
+ * Copyright 2020 Daniel Lehman
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <stdio.h>
+#include <windows.h>
+#include <initguid.h>
+
+HRESULT WINAPI DllGetClassObject(REFCLSID clsid, REFIID riid, void **obj)
+{
+    return E_NOTIMPL;
+}
