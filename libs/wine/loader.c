7f50dcdb1ff8777c1d357fcbc132b0d2e0cecbee
Daniel Lehman <dlehman25@gmail.com>
2020-06-18 22:13:21 -0700

    kinda half works

--- libs/wine/loader.c
+++ libs/wine/loader.c
@@ -977,6 +977,97 @@ jobject wine_get_java_object(void)
     return java_object;
 }
 
+static jstring wine_main_jni( JNIEnv *env, jobject obj, jobjectArray cmdline, jobjectArray environment )
+{
+    char **argv;
+    char *str;
+    char error[1024];
+    int i, argc, length;
+
+    /* get the command line array */
+    argc = (*env)->GetArrayLength( env, cmdline );
+    for (i = length = 0; i < argc; i++)
+    {
+        jobject str_obj = (*env)->GetObjectArrayElement( env, cmdline, i );
+        length += (*env)->GetStringUTFLength( env, str_obj ) + 1;
+    }
+
+    argv = malloc( (argc + 1) * sizeof(*argv) + length );
+    str = (char *)(argv + argc + 1);
+    for (i = 0; i < argc; i++)
+    {
+        jobject str_obj = (*env)->GetObjectArrayElement( env, cmdline, i );
+        length = (*env)->GetStringUTFLength( env, str_obj );
+        (*env)->GetStringUTFRegion( env, str_obj, 0,
+                                    (*env)->GetStringLength( env, str_obj ), str );
+        argv[i] = str;
+        str[length] = 0;
+        str += length + 1;
+    }
+    argv[argc] = NULL;
+
+    /* set the environment variables */
+
+    if (environment)
+    {
+        int count = (*env)->GetArrayLength( env, environment );
+        for (i = 0; i < count - 1; i += 2)
+        {
+            jobject var_obj = (*env)->GetObjectArrayElement( env, environment, i );
+            jobject val_obj = (*env)->GetObjectArrayElement( env, environment, i + 1 );
+            const char *var = (*env)->GetStringUTFChars( env, var_obj, NULL );
+
+            if (val_obj)
+            {
+                const char *val = (*env)->GetStringUTFChars( env, val_obj, NULL );
+                setenv( var, val, 1 );
+                if (!strcmp( var, "LD_LIBRARY_PATH" ))
+                {
+                    void (*update_func)( const char * ) = dlsym( RTLD_DEFAULT,
+                                                                 "android_update_LD_LIBRARY_PATH" );
+                    if (update_func) update_func( val );
+                }
+                else if (!strcmp( var, "WINEDEBUGLOG" ))
+                {
+                    int fd = open( val, O_WRONLY | O_CREAT | O_APPEND, 0666 );
+                    if (fd != -1)
+                    {
+                        dup2( fd, 2 );
+                        close( fd );
+                    }
+                }
+                (*env)->ReleaseStringUTFChars( env, val_obj, val );
+            }
+            else unsetenv( var );
+
+            (*env)->ReleaseStringUTFChars( env, var_obj, var );
+        }
+    }
+
+    java_object = (*env)->NewGlobalRef( env, obj );
+    {
+        struct dll_path_context context;
+        char *path;
+        void *ntdll = NULL;
+        void (*init_func)(int, char **, char **);
+        //    wine_init_argv0_path( argv[0] );
+        build_dll_path();
+        for (path = first_dll_path( "ntdll", 0, &context ); path; path = next_dll_path( &context ))
+        {
+            if ((ntdll = dlopen( path, RTLD_NOW )))
+                break;
+        }
+        free_dll_path( &context );
+        if (ntdll)
+        {
+            init_func = dlsym( ntdll, "__wine_main" );
+            if (init_func) init_func( argc, argv, environ );
+        }
+    }
+    //wine_init( argc, argv, error, sizeof(error) );
+    return (*env)->NewStringUTF( env, error );
+}
+
 /* main Wine initialisation */
 static jstring wine_init_jni( JNIEnv *env, jobject obj, jobjectArray cmdline, jobjectArray environment )
 {
@@ -1053,7 +1144,7 @@ static jstring wine_init_jni( JNIEnv *env, jobject obj, jobjectArray cmdline, jo
         unsigned short java_fs;
         __asm__( "mov %%fs,%0" : "=r" (java_fs) );
         __asm__( "mov %0,%%fs" :: "r" (0) );
-        wine_init( argc, argv, error, sizeof(error) );
+        wine_main( argc, argv, error, sizeof(error) );
         __asm__( "mov %0,%%fs" :: "r" (java_fs) );
     }
 #else
@@ -1066,7 +1157,7 @@ jint JNI_OnLoad( JavaVM *vm, void *reserved )
 {
     static const JNINativeMethod method =
     {
-        "wine_init", "([Ljava/lang/String;[Ljava/lang/String;)Ljava/lang/String;", wine_init_jni
+        "wine_init", "([Ljava/lang/String;[Ljava/lang/String;)Ljava/lang/String;", wine_main_jni
     };
 
     JNIEnv *env;
