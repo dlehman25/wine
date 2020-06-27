bcf151fef1990f66b55134b42167a8c759765377
Daniel Lehman <dlehman25@gmail.com>
2020-06-26 19:59:59 -0700

    winesharedlib: using __wine_main

--- programs/winesharedlib/winesharedlib.c
+++ programs/winesharedlib/winesharedlib.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <windows.h>
 #include <winternl.h>
+#include <dlfcn.h>
 #include <setjmp.h>
 
 static jmp_buf jmpbuf;
@@ -13,7 +14,39 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
     longjmp( jmpbuf, 1 );
 }
 
+typedef void (*__wine_main_t)( int argc, char *argv[], char *envp[] );
+
 int SharedWineInit(void)
+{
+    char *WineArguments[2];
+    char *cmdline;
+    void *ntdll;
+    __wine_main_t __wine_main;
+
+    ntdll = dlopen(DLLPATH "/ntdll.so", RTLD_NOW);
+    printf("ntdll %p\n", ntdll);
+    __wine_main = (__wine_main_t)dlsym(ntdll, "__wine_main");
+    printf("__wine_main %p\n", __wine_main);
+
+    cmdline = strdup("sharedapp " DLLPATH "/winesharedlib.exe.so");
+    printf("%s\n", cmdline);
+    WineArguments[0] = cmdline;
+    WineArguments[1] = strstr(cmdline, LIBPATH);
+
+    if (!setjmp( jmpbuf ))
+    {
+        __wine_main(2, WineArguments, NULL);
+        printf( "should not get here\n" );
+    }
+    NtCurrentTeb()->Tib.ExceptionList = (void *)~0UL;
+    VirtualFree( NtCurrentTeb()->DeallocationStack, 0, MEM_RELEASE );
+    NtCurrentTeb()->DeallocationStack = NULL;
+    NtCurrentTeb()->Tib.StackBase = (void*)~0UL;  /* FIXME: should find actual limits here */
+    NtCurrentTeb()->Tib.StackLimit = 0;
+    return(0);
+}
+
+int SharedWineInit2(void)
 {
     char Error[1024]="";
     char *WineArguments[2];
