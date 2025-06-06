61778ae924807ae0a36bee54ff60ecc8c4b7ea51
Daniel Lehman <dlehman25@gmail.com>
2025-06-05 23:48:11 -0700

    libs/musl: Use 53-bit precision in rint.

--- libs/musl/src/math/rint.c
+++ libs/musl/src/math/rint.c
@@ -15,15 +15,28 @@ double __cdecl rint(double x)
 	union {double f; uint64_t i;} u = {x};
 	int e = u.i>>52 & 0x7ff;
 	int s = u.i>>63;
+#if defined(__GNUC__) && defined(__386__)
+ 	unsigned cw;
+#endif
 	double_t y;
 
 	if (e >= 0x3ff+52)
 		return x;
+#if defined(__GNUC__) && defined(__386__)
+	cw = _controlfp(0, 0);
+	if ((cw & _MCW_PC) != _PC_53)
+		_controlfp(_PC_53, _MCW_PC);
+#endif
 	if (s)
 		y = fp_barrier(x - toint) + toint;
 	else
 		y = fp_barrier(x + toint) - toint;
-	if (y == 0)
-		return s ? -0.0 : 0;
+	if (y == 0 && s)
+		y = -0.0;
+
+#if defined(__GNUC__) && defined(__386__)
+	if ((cw & _MCW_PC) != _PC_53)
+		_controlfp(cw, _MCW_PC);
+#endif
 	return y;
 }
