dlehman25/wine

commit a965a5c39fa6da97c3f8f0b5956c5dde3d075b8d

jscript: Treat DISPATCH_PROPERTYGET | DISPATCH_METHOD as property getter in ES5+ modes.
Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

dlls/jscript/dispex.c
@@ -2136,10 +2136,10 @@ static HRESULT WINAPI DispatchEx_InvokeEx(IWineJSDispatch *iface, DISPID id, LCI
     if(pspCaller)
         IServiceProvider_AddRef(pspCaller);
 
+    if(wFlags == (DISPATCH_METHOD | DISPATCH_PROPERTYGET))
+        wFlags = (This->ctx->version < SCRIPTLANGUAGEVERSION_ES5) ? DISPATCH_METHOD : DISPATCH_PROPERTYGET;
+
     switch(wFlags) {
-    case DISPATCH_METHOD|DISPATCH_PROPERTYGET:
-        wFlags = DISPATCH_METHOD;
-        /* fall through */
     case DISPATCH_METHOD:
     case DISPATCH_CONSTRUCT: {
         jsval_t *argv, buf[6], r;
@@ -2712,7 +2712,7 @@ HRESULT disp_call(script_ctx_t *ctx, IDispatch *disp, DISPID id, WORD flags, uns
         jsdisp_release(jsdisp);
 
     flags &= ~DISPATCH_JSCRIPT_INTERNAL_MASK;
-    if(ret && argc)
+    if(ret && argc && (!jsdisp || ctx->version < SCRIPTLANGUAGEVERSION_ES5))
         flags |= DISPATCH_PROPERTYGET;
 
     dp.cArgs = argc;

dlls/mshtml/tests/dom.c
@@ -11731,6 +11731,85 @@ static void test_case_insens(IHTMLDocument2 *doc)
     IDispatchEx_Release(dispex);
 }
 
+static void test_method_vs_getter(IHTMLDocument2 *doc)
+{
+    DISPPARAMS dp = { 0 };
+    IDispatchEx *dispex;
+    DISPID dispid;
+    HRESULT hres;
+    VARIANT v;
+    BSTR bstr;
+
+    hres = IHTMLDocument2_QueryInterface(doc, &IID_IDispatchEx, (void**)&dispex);
+    ok(hres == S_OK, "Could not get IDispatchEx: %08lx\n", hres);
+
+    V_VT(&v) = VT_EMPTY;
+    hres = IDispatchEx_InvokeEx(dispex, DISPID_VALUE, LOCALE_NEUTRAL, DISPATCH_METHOD | DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+    todo_wine_if(compat_mode < COMPAT_IE9)
+    ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
+    todo_wine_if(compat_mode < COMPAT_IE9)
+    ok(V_VT(&v) == VT_BSTR, "V_VT = %d\n", V_VT(&v));
+    VariantClear(&v);
+
+    bstr = SysAllocString(L"body");
+    hres = IDispatchEx_GetDispID(dispex, bstr, 0, &dispid);
+    ok(hres == S_OK, "GetDispID returned: %08lx\n", hres);
+    SysFreeString(bstr);
+
+    hres = IDispatchEx_InvokeEx(dispex, dispid, LOCALE_NEUTRAL, DISPATCH_METHOD | DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+    ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
+    ok(V_VT(&v) == VT_DISPATCH, "V_VT = %d\n", V_VT(&v));
+    ok(V_DISPATCH(&v) != NULL, "V_DISPATCH == NULL\n");
+    VariantClear(&v);
+
+    bstr = SysAllocString(L"title");
+    hres = IDispatchEx_GetDispID(dispex, bstr, 0, &dispid);
+    ok(hres == S_OK, "GetDispID returned: %08lx\n", hres);
+    SysFreeString(bstr);
+
+    hres = IDispatchEx_InvokeEx(dispex, dispid, LOCALE_NEUTRAL, DISPATCH_METHOD | DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+    ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
+    ok(V_VT(&v) == VT_BSTR, "V_VT = %d\n", V_VT(&v));
+    VariantClear(&v);
+
+    bstr = SysAllocString(L"close");
+    hres = IDispatchEx_GetDispID(dispex, bstr, 0, &dispid);
+    ok(hres == S_OK, "GetDispID returned: %08lx\n", hres);
+    SysFreeString(bstr);
+
+    hres = IDispatchEx_InvokeEx(dispex, dispid, LOCALE_NEUTRAL, DISPATCH_METHOD | DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+    ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
+    if(compat_mode < COMPAT_IE9)
+        ok(V_VT(&v) == VT_EMPTY, "V_VT = %d\n", V_VT(&v));
+    else {
+        ok(V_VT(&v) == VT_DISPATCH, "V_VT = %d\n", V_VT(&v));
+        ok(V_DISPATCH(&v) != NULL, "V_DISPATCH == NULL\n");
+    }
+    VariantClear(&v);
+
+    hres = IDispatchEx_InvokeEx(dispex, dispid, LOCALE_NEUTRAL, DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+    ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
+    ok(V_VT(&v) == VT_DISPATCH, "V_VT = %d\n", V_VT(&v));
+    ok(V_DISPATCH(&v) != NULL, "V_DISPATCH == NULL\n");
+    IDispatchEx_Release(dispex);
+
+    hres = IDispatch_QueryInterface(V_DISPATCH(&v), &IID_IDispatchEx, (void**)&dispex);
+    ok(hres == S_OK, "Could not get IDispatchEx: %08lx\n", hres);
+    VariantClear(&v);
+
+    hres = IDispatchEx_InvokeEx(dispex, DISPID_VALUE, LOCALE_NEUTRAL, DISPATCH_METHOD | DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+    if(compat_mode < COMPAT_IE9)
+        todo_wine
+        ok(hres == E_ACCESSDENIED, "InvokeEx returned: %08lx\n", hres);
+    else {
+        ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
+        ok(V_VT(&v) == VT_BSTR, "V_VT = %d\n", V_VT(&v));
+    }
+    VariantClear(&v);
+
+    IDispatchEx_Release(dispex);
+}
+
 static void test_null_write(IHTMLDocument2 *doc)
 {
     HRESULT hres;
@@ -13842,9 +13921,11 @@ START_TEST(dom)
     run_domtest(doc_blank_ie8, test_quirks_mode_perf_toJSON);
     run_domtest(doctype_str, test_doctype);
     run_domtest(case_insens_str, test_case_insens);
+    run_domtest(doc_blank, test_method_vs_getter);
     if(is_ie9plus) {
         compat_mode = COMPAT_IE9;
         run_domtest(emptydiv_ie9_str, test_docfrag);
+        run_domtest(doc_blank_ie9, test_method_vs_getter);
         compat_mode = COMPAT_NONE;
     }
 

commit eaea4240c4efb618be6d20c05f7fc9f3db9a104c

jscript: Always treat DISPATCH_METHOD | DISPATCH_PROPERTYGET as method call if arguments are supplied.
Fixes a regression introduced by a965a5c39fa6da97c3f8f0b5956c5dde3d075b8d
which broke the Adobe cloud installers.

Spotted by Filip Bakreski.

Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

dlls/jscript/dispex.c
@@ -2137,7 +2137,7 @@ static HRESULT WINAPI DispatchEx_InvokeEx(IWineJSDispatch *iface, DISPID id, LCI
         IServiceProvider_AddRef(pspCaller);
 
     if(wFlags == (DISPATCH_METHOD | DISPATCH_PROPERTYGET))
-        wFlags = (This->ctx->version < SCRIPTLANGUAGEVERSION_ES5) ? DISPATCH_METHOD : DISPATCH_PROPERTYGET;
+        wFlags = (This->ctx->version < SCRIPTLANGUAGEVERSION_ES5 || pdp->cArgs) ? DISPATCH_METHOD : DISPATCH_PROPERTYGET;
 
     switch(wFlags) {
     case DISPATCH_METHOD:

dlls/mshtml/tests/dom.c
@@ -11735,9 +11735,9 @@ static void test_method_vs_getter(IHTMLDocument2 *doc)
 {
     DISPPARAMS dp = { 0 };
     IDispatchEx *dispex;
+    VARIANT v, arg;
     DISPID dispid;
     HRESULT hres;
-    VARIANT v;
     BSTR bstr;
 
     hres = IHTMLDocument2_QueryInterface(doc, &IID_IDispatchEx, (void**)&dispex);
@@ -11785,6 +11785,15 @@ static void test_method_vs_getter(IHTMLDocument2 *doc)
     }
     VariantClear(&v);
 
+    dp.cArgs = 1;
+    dp.rgvarg = &arg;
+    V_VT(&arg) = VT_I4;
+    V_I4(&arg) = 42;
+    hres = IDispatchEx_InvokeEx(dispex, dispid, LOCALE_NEUTRAL, DISPATCH_METHOD | DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+    ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
+    ok(V_VT(&v) == VT_EMPTY, "V_VT = %d\n", V_VT(&v));
+
+    dp.cArgs = 0;
     hres = IDispatchEx_InvokeEx(dispex, dispid, LOCALE_NEUTRAL, DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
     ok(hres == S_OK, "InvokeEx failed: %08lx\n", hres);
     ok(V_VT(&v) == VT_DISPATCH, "V_VT = %d\n", V_VT(&v));