c1e4c043249124443e58e54acbc2a7948adf654a
Daniel Lehman <dlehman25@gmail.com>
2026-02-15 10:40:53 -0800

    fixup

--- libs/xml2/include/libxml/xmlversion.h
+++ libs/xml2/include/libxml/xmlversion.h
@@ -238,7 +238,7 @@
 /**
  * the string suffix used by dynamic modules (usually shared libraries)
  */
-#define LIBXML_MODULE_EXTENSION ".so" 
+#define LIBXML_MODULE_EXTENSION ".dll"
 #endif
 
 #if 0
