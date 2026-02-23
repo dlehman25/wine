a955417b8f7bc959ee2f256ece1c73ee71c20f60
Piotr Caban <piotr@codeweavers.com>
2026-02-23 11:53:27 +0100

    msvcr100: Fix leak in __ExceptionPtrAssign.

--- dlls/msvcrt/exception_ptr.c
+++ dlls/msvcrt/exception_ptr.c
@@ -116,10 +116,7 @@ void __cdecl __ExceptionPtrAssign(exception_ptr *ep, const exception_ptr *assign
 {
     TRACE("(%p %p)\n", ep, assign);
 
-    /* don't destroy object stored in ep */
-    if (ep->ref)
-        InterlockedDecrement(ep->ref);
-
+    __ExceptionPtrDestroy(ep);
     *ep = *assign;
     if (ep->ref)
         InterlockedIncrement(ep->ref);
